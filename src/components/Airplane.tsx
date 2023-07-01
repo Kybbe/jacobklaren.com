65ab7574fcd7d2f25488e2315a5243e9e3df5874
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2023-07-01 18:59:39 +0200

    add fun airplane Spline 3d thing

---
+++ src/components/Airplane.tsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function Airplane() {
+	return (
+		<Spline scene="https://prod.spline.design/INTLvlbGTeY8d9sL/scene.splinecode" />
+	);
+}
