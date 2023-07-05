5df501b9539ecf91d887a12c61443380017861fe
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2023-07-05 17:44:24 +0200

    move + change airplane

--- src/components/Contact/Airplane.tsx
+++ src/components/Contact/Airplane.tsx
@@ -2,6 +2,6 @@ import Spline from "@splinetool/react-spline";
 
 export default function Airplane() {
 	return (
-		<Spline scene="https://prod.spline.design/INTLvlbGTeY8d9sL/scene.splinecode" />
+		<Spline scene="https://prod.spline.design/6kGA7xQY4TeAfm-X/scene.splinecode" />
 	);
 }
